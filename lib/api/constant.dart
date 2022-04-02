78ba28c7fa3033146c84015b99550fb4ef703a08
pulkit321 <pulkitdhirana3@gmail.com>
2022-04-02 18:42:47 +0530

    Updated UI

--- lib/api/constant.dart
+++ lib/api/constant.dart
@@ -1,5 +1,5 @@
 const String baseUrl="http://cardicon2020.com/api";
-const String validateUser="$baseUrl/login";
+const String validateUser="https://farmer.cropnet.co.in/api/login";
 const String searchQuery="$baseUrl/search";
 const String verifyOtp="$baseUrl/forget_password";
 const String registeration="$baseUrl/new_register";
